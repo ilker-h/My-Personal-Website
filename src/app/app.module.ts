10deb30627fb6208a026d5e4f76f93a9c4d9bf9a
ilker-h <ilker.hadzhalaran@gmail.com>
2020-05-09 20:22:38 -0400

    download Bootstrap and Angular Material. Make header and hamburger menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OriginalAngularPageComponent } from './original-angular-page/original-angular-page.component';
+import { HeaderComponent } from './header/header.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OriginalAngularPageComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSliderModule,
+    MatIconModule,
+    MatMenuModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
