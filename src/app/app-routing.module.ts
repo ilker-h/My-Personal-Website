8cb64b9ac6fb5a0f1f92917dd9f4ed257a87ed7c
ilker-h <ilker.hadzhalaran@gmail.com>
2020-05-20 00:31:05 -0400

    make CSS on 'Skills' section more responsive on mobile screens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-// import { AboutComponent } from './about/about.component';
-// import { SkillsComponent } from './skills/skills.component';
-// import { ExperienceComponent } from './experience/experience.component';
-// import { ProjectsComponent } from './projects/projects.component';
-// import { ContactComponent } from './contact/contact.component';
-
-const routes: Routes = [
-  // { path: '', component: AboutComponent, pathMatch: 'full'},
-  // { path: '', component: SkillsComponent},
-  // { path: '', component: ExperienceComponent},
-  // { path: '', component: ProjectsComponent},
-  // { path: '', component: ContactComponent},
-  // { path: '**', redirectTo: '', pathMatch: 'full'}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, anchorScrolling: 'enabled' })],
